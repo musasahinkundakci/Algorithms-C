fddfe644e57cea9ae1fa3381f02450132c0ee674
Musa Şahin Kundakcı <55662417+musasahinkundakci@users.noreply.github.com>
2021-04-22 17:40:20 +0300

    Create basic-linked.c

---
+++ basic-linked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct{
+  int school_no,gpa;
+  char name[30];
+  struct student *next;
+}student;
+int main(void) {
+  student *ptrfirst;
+  ptrfirst=malloc(sizeof(student));
+  ptrfirst->school_no=2019555049;
+  ptrfirst->gpa=90;
+  strcpy(ptrfirst->name,"Musa Şahin Kundakcı");
+  ptrfirst->next=malloc(sizeof(student));
+  ptrfirst->next->next=malloc(sizeof(student));
+
+  
+  return 0;
+}
