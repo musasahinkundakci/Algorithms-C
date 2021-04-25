aec8a79899777d5c0d9bcae0af3ec6d5b4d9f162
Musa Şahin Kundakcı <55662417+musasahinkundakci@users.noreply.github.com>
2021-04-25 15:39:56 +0300

    Create structers_notes2.c

---
+++ structers_notes2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma pack(1)//For not wasting memory
+//because when we define a structure if it s 32 bit
+//the rooms are just 4 bytes and if the structer is like
+//inta char b int c 
+//there would be 12 byte
+
+int main(void) {
+  
+
+  return 0;
+}
