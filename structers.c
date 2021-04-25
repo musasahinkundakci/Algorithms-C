95da8516700a085b9df1eef9b63c97608e4563e7
Musa Şahin Kundakcı <55662417+musasahinkundakci@users.noreply.github.com>
2021-04-25 14:05:50 +0300

    Update structers.c

--- structers.c
+++ structers.c
@@ -5,7 +5,22 @@ typedef struct{
   float gpa;
   char name[50];
    }student;
+//************************************//
 int main(void) {
+  struct{
+    char *a;
+  }car1,car2;
+  struct employee{
+    int salary;
+    char *name;
+  }employee2;
+  struct employee employee1;
+  employee1.name="Musa";
+  employee1.salary=2000;
+  employee2.salary=5000;
+  
+  
+  //******************************************//
   student number1;
   number1.first=50;
   number1.second=100;
