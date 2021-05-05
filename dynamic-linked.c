1b0490ae415e232b750003a5ecdaaca3344f405e
Musa Şahin Kundakcı <55662417+musasahinkundakci@users.noreply.github.com>
2021-05-05 21:13:39 +0300

    Update dynamic-linked.c

--- dynamic-linked.c
+++ dynamic-linked.c
@@ -1,137 +1,108 @@
-//Not my code 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-typedef struct listNode{
-  char data;
-  struct listNode *nextPtr;
-}ListNode_t;
-void insert(ListNode_t **sPtr ,char value);
-char delete(ListNode_t **sPtr,char value);
-int isEmpty(ListNode_t *sPtr);
-void printList(ListNode_t *currentPtr);
-void instructions(void);
+typedef struct student{
+  int id;
+  struct student *next;
+}Student;
+void insert(Student **head);
+void display();
+void delete(Student **head);
 int main(void) {
-  ListNode_t  *startPtr=NULL;
-  int choice=0;
-  char item=0;
-
-  instructions();
-  printf("? ");
-  scanf("%d",&choice);
-  while(choice!=3){
-    switch(choice){
-    case 1:
-      printf("Enter a character: ");
-      scanf("\n%c",&item);
-      insert(&startPtr,item);
-      printList(startPtr);
-      break;
-    
-    case 2:
-      if(!isEmpty(startPtr)){
-        printf("Enter character to be deleted: ");
-        scanf("\n%c",&item);
-        if(delete(&startPtr,item)){
-          printf("%c deleted.\n",item);
-          printList(startPtr);
-        }
-        else{
-          printf("%c not found.\n\n",item);
-        }
-      }
-      else{
-        printf("List is empty.\n\n ");
-      }
-      break;
-    default:
-      printf("Invalid choice\n\n");
-      break;
-    }
-    instructions();
-    printf("? ");
+  Student *startptr;
+  startptr=NULL;
+  int choice;
+  while(1){
+    printf("1-insert 2-Delete 3-Display:\n?");
     scanf("%d",&choice);
+    switch(choice){
+      case 1:{
+        insert(&startptr);
+        break;
+      }
+      case 2:{
+        delete(&startptr);
+        break;
+      }
+      case 3:{
+        display(&startptr);
+        break;
+      }
+    }
   }
-  printf("End of the run.\n");
   return 0;
 }
-void instructions(void){
-  printf("Enter your choice:\n"
-    " 1 to insert an element into the list\n"
-    " 2 to delete an element from the list\n"
-    " 3 to end.\n");
-
-}
-void insert(ListNode_t **sPtr,char value){
-  ListNode_t *newPtr=NULL;
-  ListNode_t *previousPtr=NULL;
-  ListNode_t *currentPtr=NULL;
-  newPtr=malloc(sizeof(ListNode_t));
-  if(newPtr != NULL){
-    newPtr->data=value;
-    newPtr->nextPtr=NULL;
-    previousPtr=NULL;
-    currentPtr=*sPtr;
-    while(currentPtr!=NULL && value>currentPtr->data){
-      previousPtr=currentPtr;
-      currentPtr=currentPtr->nextPtr;
+void insert(Student **head){
+  Student *newPtr=NULL;
+  Student *current=NULL;
+  Student *previous=NULL;
+  newPtr=malloc(sizeof(Student));
+  int id;
+  printf("ID:\n?");
+  scanf("%d",&id);
+  newPtr->id=id;
+  newPtr->next=NULL;
+  if((*head)==NULL){
+    (*head)=newPtr;
+  }
+  else{
+    
+    current=*head;
+    while(current!=NULL && id > current->id){
+      previous=current;
+      current=current->next;
     }
-    if(previousPtr==NULL){
-      newPtr->nextPtr=*sPtr;
-      *sPtr=newPtr;
+    if(previous==NULL){
+      newPtr->next=current;
+      (*head)=newPtr;
     }
     else{
-      previousPtr->nextPtr=newPtr;
-      newPtr->nextPtr=currentPtr;
-  }
-  } 
-  else{
-    printf("%c not inserted.No memory available.\n",value);
-  }
-}
-char delete(ListNode_t **sPtr,char value){
-  ListNode_t *previousPtr=NULL;
-  ListNode_t *currentPtr=NULL;
-  ListNode_t *tempPtr=NULL;
-
-  if(value==(*sPtr)->data){
-    tempPtr=*sPtr;
-    *sPtr=(*sPtr)->nextPtr;
-    free(tempPtr);
-    return value;
-  }
-  else{
-    previousPtr=*sPtr;
-    currentPtr=(*sPtr)->nextPtr;
-    while(currentPtr!=NULL && currentPtr->data!=value){
-      previousPtr=currentPtr;
-      currentPtr=currentPtr->nextPtr;
+      //printf("Debugger!\n");
+      previous->next=newPtr;
+   
+      newPtr->next=current;
     }
-    if(currentPtr!=NULL){
-      tempPtr=currentPtr;
-      previousPtr->nextPtr=currentPtr->nextPtr;
-      free(tempPtr);
-      return value;
-    }
-  }
-  return '\0';
-}
-int isEmpty(ListNode_t *sPtr){
-  return sPtr==NULL;
-}
-void printList(ListNode_t *currentPtr){
-  if(currentPtr==NULL){
-    printf("List is empty\n");
 
+  }
+  printf("Element is added!\n");
+}
+void delete(Student **head){
+  int id;
+  Student *previous=NULL;
+  Student *current=NULL;
+  Student *temp;
+  printf("Element's ID:");
+  scanf("%d",&id);
+  current=*head;
+  while(current!=NULL && current->id != id){
+    previous=current;
+    current=current->next;
+  }
+  if(previous==NULL && (*head)->id ==id){
+    temp=*head;
+    (*head)=(*head)->next;
+    free(temp);
+    printf("Element is deleted");
+    
   }
   else{
-    printf("The list is:\n");
-
-    while(currentPtr!=NULL){
-      printf("%c -->",currentPtr->data);
-      currentPtr=currentPtr->nextPtr;
+    if(current->next != NULL){
+      previous->next=current->next;
+      free(current);
+      printf("Element is deleted");
     }
-    printf("NULL\n\n");
-
+    else{
+      previous->next=NULL;
+      free(current);
+      printf("Element is deleted");
+    }
   }
+}
+void display(Student **head){
+  Student *temp;
+  temp=*head;
+  while(temp!=NULL){
+    printf("%d -|- ",temp->id);
+    temp=temp->next;
+  }
+  printf("\n");
 }
